e9b8e66ac31ba065b7b37c52ff9a1406e1056fc3
James Orson <jamesaorson@gmail.com>
2025-06-30 18:21:18 -0700

    Only render existing pages
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -7,13 +7,8 @@
 <script setup lang="ts">
 // Makes sure the markdown pages are rendered before the generation is done
 prerenderRoutes([
-  "/docs/api-reference",
-  "/docs/configuration",
-  "/docs/examples",
   "/docs/getting-started",
   "/docs/help",
-  "/docs/install",
-  "/docs/quick-start",
   "/docs/welcome",
 ]);
 await navigateTo("/docs/welcome");
